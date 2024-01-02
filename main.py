9f842d539e323a84279022a0a69c7a6f3a1e7a68
devabacus <devabacusnrf@gmail.com>
2024-01-02 12:37:13 +0500

    use one window for pick snippet and for descri

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pynput.keyboard import Listener
+from pynput.keyboard import Key, Controller
 import tkinter as tk
 import pyautogui
 
@@ -7,6 +8,8 @@ snippets = {
     "Snippet 1": "def function_name():\n    pass",
     "Snippet 2": "class ClassName:\n    def __init__(self):\n        pass",
 }
+
+keyboard_controller = Controller()
 
 def on_press(key):
     global typed_keys
@@ -27,13 +30,42 @@ def on_select(event):
         selected_snippet = listbox.get(index)
         update_snippet_content(selected_snippet)
 
-def insert_snippet():
+def insert_snippet1():
     index = listbox.curselection()
     if index:
         selected_snippet = listbox.get(index)
         pyautogui.hotkey('alt', 'tab')
         pyautogui.press('backspace', presses=4)
         pyautogui.write(snippets[selected_snippet])
+        popup.destroy()
+        
+        
+def insert_snippet():
+    index = listbox.curselection()
+    if index:
+        selected_snippet = listbox.get(index)
+        snippet_text = snippets[selected_snippet]
+
+        # Имитация Alt+Tab для смены фокуса на окно редактора
+        keyboard_controller.press(Key.alt)
+        keyboard_controller.press(Key.tab)
+        keyboard_controller.release(Key.tab)
+        keyboard_controller.release(Key.alt)
+
+        # Удаление триггера 'trig'
+        for _ in range(5):
+            keyboard_controller.press(Key.backspace)
+            keyboard_controller.release(Key.backspace)
+        
+        # Ввод сниппета
+        for char in snippet_text:
+            if char == "\n":
+                keyboard_controller.press(Key.enter)
+                keyboard_controller.release(Key.enter)
+            else:
+                keyboard_controller.press(char)
+                keyboard_controller.release(char)
+        
         popup.destroy()
 
 def show_popup():
